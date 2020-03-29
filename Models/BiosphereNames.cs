b3f33ef6b7c50965c5bd0cc15e9b84a4c6c6bada
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-03-29 18:46:34 +0300

    Added Animal namelists

---
+++ Models/BiosphereNames.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace StellarisNameListGenerator.Models
+{
+    public sealed class BiosphereNames
+    {
+        public List<NameGroup> Animals { get; set; }
+        public List<NameGroup> MythologicalCreatures { get; set; }
+
+        public BiosphereNames()
+        {
+            Animals = new List<NameGroup>();
+            MythologicalCreatures = new List<NameGroup>();
+        }
+    }
+}
