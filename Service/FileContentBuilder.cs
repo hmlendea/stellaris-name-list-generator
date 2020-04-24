cdab0b9352ce9d0f8f1e760057b54655161d8e22
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-04-24 21:31:33 +0300

    Removed unnecessary parallelisation

--- Service/FileContentBuilder.cs
+++ Service/FileContentBuilder.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 using NuciExtensions;
 
@@ -41,8 +39,6 @@ namespace StellarisNameListGenerator.Service
 
         public string BuildContent(NameList nameList)
         {
-            Dictionary<string, string> sections = GenerateNameListSections(nameList);
-
             string content = $"### {nameList.Id}{Environment.NewLine}";
             content += $"### {nameList.Name}{Environment.NewLine}";
             content += $"### Leaders: {characterNamesBuilder.GetRandomName(nameList)}, {characterNamesBuilder.GetRandomName(nameList)}{Environment.NewLine}";
@@ -54,61 +50,16 @@ namespace StellarisNameListGenerator.Service
             content += $"{nameList.Id} = {{{Environment.NewLine}";
             content += BuildRandomisableOption(nameList.IsLocked);
 
-            content += sections["ShipNames"];
-            content += sections["ShipClassNames"];
-            content += sections["FleetNames"];
-            content += sections["ArmyNames"];
-            content += sections["PlanetNames"];
-            content += sections["CharacterNames"];
+            content += shipNamesBuilder.Build(nameList);
+            content += shipClassNamesBuilder.Build(nameList);
+            content += fleetNamesBuilder.Build(nameList);
+            content += armyNamesBuilder.Build(nameList);
+            content += planetNamesBuilder.Build(nameList);
+            content += characterNamesBuilder.Build(nameList);
 
             content += $"}}{Environment.NewLine}";
 
             return content;
-        }
-
-        Dictionary<string, string> GenerateNameListSections(NameList nameList)
-        {
-            Dictionary<string, string> sections = new Dictionary<string, string>()
-            {
-                { "ShipNames", string.Empty },
-                { "ShipClassNames", string.Empty },
-                { "FleetNames", string.Empty },
-                { "ArmyNames", string.Empty },
-                { "PlanetNames", string.Empty },
-                { "CharacterNames", string.Empty }
-            };
-
-            Parallel.ForEach(sections, section =>
-            {
-                switch (section.Key)
-                {
-                    case "ShipNames":
-                        sections[section.Key] = shipNamesBuilder.Build(nameList);
-                        break;
-
-                    case "ShipClassNames":
-                        sections[section.Key] = shipClassNamesBuilder.Build(nameList);
-                        break;
-
-                    case "FleetNames":
-                        sections[section.Key] = fleetNamesBuilder.Build(nameList);
-                        break;
-
-                    case "ArmyNames":
-                        sections[section.Key] = armyNamesBuilder.Build(nameList);
-                        break;
-
-                    case "PlanetNames":
-                        sections[section.Key] = planetNamesBuilder.Build(nameList);
-                        break;
-
-                    case "CharacterNames":
-                        sections[section.Key] = characterNamesBuilder.Build(nameList);
-                        break;
-                }
-            });
-
-            return sections;
         }
 
         string BuildRandomisableOption(bool isLocked)
