912dd46de8c8f1453d3701ae45cc916b8ed63703
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-03-30 13:56:59 +0300

    deps: Updated the code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using NuciCLI;
+﻿using System;
+using System.Collections.Generic;
+
+using NuciCLI.Arguments;
 using NuciDAL.Repositories;
 
 using StellarisNameListGenerator.Models;
@@ -9,10 +12,10 @@ namespace StellarisNameListGenerator
 {
     public class Program
     {
-        private static readonly string[] InputFileOptions = ["-i", "--input"];
-        private static readonly string[] OutputFileOptions = ["-o", "--output"];
-        private static readonly string[] NameOptions = ["-n", "--name"];
-        private static readonly string[] IsLockedOptions = ["-l", "--locked"];
+        private const string InputArgument = "input";
+        private const string OutputArgument = "output";
+        private const string NameArgument = "name";
+        private const string LockedArgument = "locked";
 
         /// <summary>
         /// The entry point of the program, where the program control starts and ends.
@@ -20,10 +23,20 @@ namespace StellarisNameListGenerator
         /// <param name="args">CLI arguments</param>
         public static void Main(string[] args)
         {
-            string inputFilePath = CliArgumentsReader.GetOptionValue(args, InputFileOptions);
-            string outputFilePath = CliArgumentsReader.GetOptionValue(args, OutputFileOptions);
-            string namelistName = CliArgumentsReader.GetOptionValue(args, NameOptions);
-            bool isLocked = CliArgumentsReader.HasOption(args, IsLockedOptions);
+            string[] normalizedArgs = NormalizeArguments(args);
+
+            ArgumentParser argumentParser = new();
+            argumentParser.AddArgument(InputArgument, required: true);
+            argumentParser.AddArgument(OutputArgument, required: true);
+            argumentParser.AddArgument(NameArgument, required: true);
+            argumentParser.AddArgument(LockedArgument, defaultValue: false);
+
+            ArgumentsCollection parsedArgs = argumentParser.ParseArgs(normalizedArgs);
+
+            string inputFilePath = parsedArgs.Get<string>(InputArgument);
+            string outputFilePath = parsedArgs.Get<string>(OutputArgument);
+            string namelistName = parsedArgs.Get<string>(NameArgument);
+            bool isLocked = ReadBool(parsedArgs.Get<object>(LockedArgument));
 
             IShipNamesBuilder shipNamesBuilder = new ShipNamesBuilder();
             IShipClassNamesBuilder shipClassNamesBuilder = new ShipClassNamesBuilder();
@@ -40,10 +53,80 @@ namespace StellarisNameListGenerator
                 planetNamesBuilder,
                 characterNamesBuilder);
 
-            IRepository<NameList> nameListRepository = new XmlRepository<NameList>(inputFilePath);
+            IFileRepository<NameList> nameListRepository = new XmlRepository<NameList>(inputFilePath);
             INameListGenerator nameListGenerator = new NameListGenerator(fileContentBuilder, nameListRepository);
 
             nameListGenerator.Generate(outputFilePath, namelistName, isLocked);
         }
+
+        private static string[] NormalizeArguments(string[] args)
+        {
+            List<string> normalized = [];
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                string current = args[i];
+
+                if (current is "-i" or "--input")
+                {
+                    normalized.Add($"--{InputArgument}");
+                    normalized.Add(ReadNextValue(args, ref i, InputArgument));
+                    continue;
+                }
+
+                if (current is "-o" or "--output")
+                {
+                    normalized.Add($"--{OutputArgument}");
+                    normalized.Add(ReadNextValue(args, ref i, OutputArgument));
+                    continue;
+                }
+
+                if (current is "-n" or "--name")
+                {
+                    normalized.Add($"--{NameArgument}");
+                    normalized.Add(ReadNextValue(args, ref i, NameArgument));
+                    continue;
+                }
+
+                if (current is "-l" or "--locked")
+                {
+                    normalized.Add($"--{LockedArgument}");
+
+                    if (i + 1 < args.Length && !args[i + 1].StartsWith('-'))
+                    {
+                        normalized.Add(args[++i]);
+                    }
+                    else
+                    {
+                        normalized.Add("true");
+                    }
+
+                    continue;
+                }
+
+                normalized.Add(current);
+            }
+
+            return [.. normalized];
+        }
+
+        private static string ReadNextValue(string[] args, ref int index, string argumentName)
+        {
+            if (index + 1 >= args.Length)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+
+            return args[++index];
+        }
+
+        private static bool ReadBool(object value)
+            => value switch
+            {
+                bool boolValue => boolValue,
+                string stringValue when bool.TryParse(stringValue, out bool parsedValue) => parsedValue,
+                null => false,
+                _ => throw new ArgumentException($"Invalid boolean value: '{value}'")
+            };
     }
 }
